8b5f501c493b4c94bbb90a243818b4f9debadd63
skyme32 <mmejiasg@zoho.com>
2021-06-03 18:18:58 +0200

    Add Material design

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.*
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -26,20 +30,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NewsStory() {
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Image(
-            painter = painterResource(R.drawable.header),
-            contentDescription = stringResource(R.string.desc_image),
-            modifier = Modifier.height(200.dp).fillMaxWidth(),
-            contentScale = ContentScale.Crop
-        )
-        Spacer(Modifier.height(16.dp))
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.header),
+                contentDescription = stringResource(R.string.desc_image),
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(Modifier.height(16.dp))
 
-        Text("Un dia fenomenal")
-        Text("Barcelona, Catalunya")
-        Text("Junio 2021")
+            Text(stringResource(R.string.text_desc),
+                style = typography.h6,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis)
+            Text(stringResource(R.string.text_location),
+                style = typography.body2)
+            Text(stringResource(R.string.text_date),
+                style = typography.body2)
+        }
     }
 }
 
